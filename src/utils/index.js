ac7432d19234bf309225c2b694f6d6cf858c4bb9
insane <luojieyy@gmail.com>
2019-09-24 23:02:58 +0800

    fix: add ecma version

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,14 @@
-
 export const promisify = (func, options) => {
   const arg = (success, fail) =>
-      func({
-          ...options,
-          success,
-          fail
-      });
+    func({
+      ...options,
+      success,
+      fail
+    });
   return new Promise(arg);
 };
 
 export const delay = time =>
-    new Promise(resolve => {
-        setTimeout(resolve, time);
-    });
+  new Promise(resolve => {
+    setTimeout(resolve, time);
+  });
