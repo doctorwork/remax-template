12aa388df9c7607b2b768d53a541ab47d374947b
insane <luojieyy@gmail.com>
2019-09-26 11:02:27 +0800

    feat: 添加首页 model

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,9 @@
 import baseModel from "./base";
 import businessModel from "./business";
-
+import pageModels from "./pages";
 // import model from every page
 
 export default {
-  models: [...baseModel, ...businessModel],
+  models: [...baseModel, ...businessModel, ...pageModels],
   hooks: {}
 };
---
+++ src/models/pages/index.js
@@ -0,0 +1,6 @@
+// import model from every page @TODO
+import indexPage from "@/pages/index/model";
+
+// import model for subpags @TODO
+
+export default [indexPage];
